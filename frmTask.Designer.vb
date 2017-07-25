3d333de5474562a21b8a02cc3769015c0c3245d9
priyagovindarajan <30402816+priyagovindarajan@users.noreply.github.com>
2017-07-25 15:09:22 +0800

    Add files via upload

---
+++ frmTask.Designer.vb
@@ -0,0 +1,271 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTask
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.txtDesc = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtname = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.btnclear = New System.Windows.Forms.Button()
+        Me.btncreate = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnclr = New System.Windows.Forms.Button()
+        Me.dgvtask = New System.Windows.Forms.DataGridView()
+        Me.colid = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colname = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colDesc = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        CType(Me.dgvtask, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.41406!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.58594!))
+        Me.TableLayoutPanel1.Controls.Add(Me.txtDesc, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label2, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.txtname, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.dgvtask, 0, 3)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 4
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.621951!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.926829!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.89024!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 72.2561!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(402, 328)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'txtDesc
+        '
+        Me.txtDesc.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtDesc.Location = New System.Drawing.Point(101, 28)
+        Me.txtDesc.Name = "txtDesc"
+        Me.txtDesc.Size = New System.Drawing.Size(298, 20)
+        Me.txtDesc.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 4)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name :"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(3, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Description :"
+        '
+        'txtname
+        '
+        Me.txtname.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtname.Location = New System.Drawing.Point(101, 3)
+        Me.txtname.Name = "txtname"
+        Me.txtname.Size = New System.Drawing.Size(298, 20)
+        Me.txtname.TabIndex = 2
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 4
+        Me.TableLayoutPanel1.SetColumnSpan(Me.TableLayoutPanel2, 2)
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.btnclear, 2, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.btncreate, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.btnEdit, 1, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.btnclr, 3, 0)
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 54)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(396, 33)
+        Me.TableLayoutPanel2.TabIndex = 7
+        '
+        'btnclear
+        '
+        Me.btnclear.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnclear.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclear.Location = New System.Drawing.Point(201, 3)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(93, 27)
+        Me.btnclear.TabIndex = 6
+        Me.btnclear.Text = "Delete"
+        Me.btnclear.UseVisualStyleBackColor = True
+        '
+        'btncreate
+        '
+        Me.btncreate.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btncreate.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncreate.Location = New System.Drawing.Point(3, 3)
+        Me.btncreate.Name = "btncreate"
+        Me.btncreate.Size = New System.Drawing.Size(93, 27)
+        Me.btncreate.TabIndex = 4
+        Me.btncreate.Text = "Create"
+        Me.btncreate.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnEdit.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.Location = New System.Drawing.Point(102, 3)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(93, 27)
+        Me.btnEdit.TabIndex = 5
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnclr
+        '
+        Me.btnclr.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnclr.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclr.Location = New System.Drawing.Point(300, 3)
+        Me.btnclr.Name = "btnclr"
+        Me.btnclr.Size = New System.Drawing.Size(93, 27)
+        Me.btnclr.TabIndex = 7
+        Me.btnclr.Text = "Clear"
+        Me.btnclr.UseVisualStyleBackColor = True
+        '
+        'dgvtask
+        '
+        Me.dgvtask.AllowUserToAddRows = False
+        Me.dgvtask.AllowUserToDeleteRows = False
+        Me.dgvtask.AllowUserToResizeRows = False
+        Me.dgvtask.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.SkyBlue
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgvtask.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvtask.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvtask.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colid, Me.colname, Me.colDesc})
+        Me.TableLayoutPanel1.SetColumnSpan(Me.dgvtask, 2)
+        Me.dgvtask.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvtask.EnableHeadersVisualStyles = False
+        Me.dgvtask.Location = New System.Drawing.Point(3, 93)
+        Me.dgvtask.Name = "dgvtask"
+        Me.dgvtask.ReadOnly = True
+        Me.dgvtask.RowHeadersVisible = False
+        Me.dgvtask.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvtask.Size = New System.Drawing.Size(396, 232)
+        Me.dgvtask.TabIndex = 8
+        '
+        'colid
+        '
+        Me.colid.HeaderText = "ID"
+        Me.colid.Name = "colid"
+        Me.colid.ReadOnly = True
+        Me.colid.Visible = False
+        '
+        'colname
+        '
+        Me.colname.HeaderText = "Name"
+        Me.colname.Name = "colname"
+        Me.colname.ReadOnly = True
+        '
+        'colDesc
+        '
+        Me.colDesc.HeaderText = "Description"
+        Me.colDesc.Name = "colDesc"
+        Me.colDesc.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.HeaderText = "ID"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.Width = 131
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Name"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.Width = 131
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Description"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        Me.DataGridViewTextBoxColumn3.Width = 131
+        '
+        'frmTask
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(402, 328)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "frmTask"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Task"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        CType(Me.dgvtask, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents txtDesc As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtname As System.Windows.Forms.TextBox
+    Friend WithEvents TableLayoutPanel2 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents btnclear As System.Windows.Forms.Button
+    Friend WithEvents btncreate As System.Windows.Forms.Button
+    Friend WithEvents btnEdit As System.Windows.Forms.Button
+    Friend WithEvents dgvtask As System.Windows.Forms.DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents colid As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents colname As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents colDesc As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents btnclr As System.Windows.Forms.Button
+End Class
---
+++ frmTask.vb
@@ -0,0 +1,162 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class frmTask
+    Dim strSql As String = ""
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+
+#Region "Create task"
+    Private Sub btncreate_Click(sender As Object, e As EventArgs) Handles btncreate.Click
+        If btncreate.Text = "Create" Then
+            txtname.Text = ""
+            txtDesc.Text = ""
+            btncreate.Text = "&Add"
+        ElseIf txtname.Text = "" Or txtDesc.Text = "" Then
+            MessageBox.Show("Cannot Add Empty!")
+
+        ElseIf btncreate.Text = "&Add" Then
+
+            For Each rowx As DataGridViewRow In dgvtask.Rows
+                If rowx.Cells("colname").Value = txtname.Text Then
+                    MessageBox.Show("Name exists already, Please enter another Name")
+                    Exit Sub
+                End If
+            Next
+
+            Dim con As New SqlConnection
+            Dim cmd As New SqlCommand
+
+            Try
+                con.ConnectionString = "Server=PCPRIYA;Database=ASSIGNMENT;Trusted_Connection=Yes;Connect Timeout=100"
+                con.Open()
+                cmd.Connection = con
+                cmd.CommandText = "insert into Task (Name,Description,datecreated,dateupdated,active) values ('" & txtname.Text.Trim.Replace("'", "''").ToString & "', '" & txtDesc.Text.Trim.Replace("'", "''").ToString & "','" & Format(DateTime.Now, "yyyy-MM-dd HH:mm:ss") & "','" & Format(DateTime.Now, "yyyy-MM-dd HH:mm:ss") & "',1)"
+                cmd.ExecuteNonQuery()
+
+                MessageBox.Show("Name and Description Created Successfully!")
+
+            Catch ex As Exception
+                MessageBox.Show("Error while inserting record on table..." & ex.Message, "Insert Records")
+            Finally
+                con.Close()
+            End Try
+        End If
+
+        LoadDgv_Task()
+
+    End Sub
+#End Region
+
+#Region "Load_Dgv (List task)"
+    Private Sub frmTask_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadDgv_Task()
+    End Sub
+
+    Private Sub LoadDgv_Task()
+
+        dgvtask.Rows.Clear()
+        Dim dttask As DataTable
+        strSql = "Select ID,Name,Description,datecreated,dateupdated,active from task"
+
+        Try
+            Dim conn As New SqlConnection("Server=PCPRIYA;Database=ASSIGNMENT;Trusted_Connection=Yes;Connect Timeout=100")
+            Dim comm As New SqlCommand()
+            Dim objDataReader As SqlDataReader
+            comm.Connection = conn
+            Dim myadapter As New SqlDataAdapter(comm)
+
+            conn.Open()
+            comm.CommandText = strSql
+            objDataReader = comm.ExecuteReader()
+            objDataReader.Close()
+            Dim myset As New DataSet
+            Dim dttDatatable As New DataTable
+            myadapter.Fill(myset, "table")
+            conn.Close()
+            dttask = myset.Tables("table")
+
+
+        Catch ex As Exception
+            Dim strErrorMessage = ex.Message
+
+
+        End Try
+
+        If dttask.Rows.Count <> 0 Then
+            For i = 0 To dttask.Rows.Count - 1
+                dgvtask.Rows.Add(dttask.Rows(i).Item("id").ToString, dttask.Rows(i).Item("Name").ToString, dttask.Rows(i).Item("Description").ToString, dttask.Rows(i).Item("active").ToString)
+            Next
+        End If
+    End Sub
+#End Region
+   
+#Region "Edit task"
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+        Try
+            con.ConnectionString = "Server=PCPRIYA;Database=ASSIGNMENT;Trusted_Connection=Yes;Connect Timeout=100"
+            con.Open()
+            cmd.Connection = con
+            cmd.CommandText = "update Task set Name = '" & txtname.Text.Trim.Replace("'", "''").ToString & "',Description = '" & txtDesc.Text.Trim.Replace("'", "''").ToString & "',dateupdated = '" & Format(DateTime.Now, "yyyy-MM-dd HH:mm:ss") & "' where ID = '" & dgvtask.CurrentRow.Cells("colID").Value.ToString & "'"
+            cmd.ExecuteNonQuery()
+
+            MessageBox.Show("Updated Successfully!")
+        Catch ex As Exception
+            MessageBox.Show("Error while Editing record on table..." & ex.Message, "Insert Records")
+        Finally
+            con.Close()
+        End Try
+
+        dgvtask.Rows(dgvtask.CurrentRow.Index).Cells("colname").Value = txtname.Text
+        dgvtask.Rows(dgvtask.CurrentRow.Index).Cells("colDesc").Value = txtDesc.Text
+        'LoadDgv_Task()
+
+    End Sub
+#End Region
+  
+#Region "Delete task"
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+        Try
+            con.ConnectionString = "Server=PCPRIYA;Database=ASSIGNMENT;Trusted_Connection=Yes;Connect Timeout=100"
+            con.Open()
+            cmd.Connection = con
+            cmd.CommandText = "Delete from Task where ID = '" & dgvtask.CurrentRow.Cells("colID").Value.ToString & "'"
+            cmd.ExecuteNonQuery()
+
+            MessageBox.Show("Deleted Successfully!")
+        Catch ex As Exception
+            MessageBox.Show("Error while deleting record on table..." & ex.Message, "Insert Records")
+        Finally
+            con.Close()
+        End Try
+
+        LoadDgv_Task()
+
+    End Sub
+#End Region
+
+#Region "Clear and Cell click"
+    Private Sub btnclr_Click(sender As Object, e As EventArgs) Handles btnclr.Click
+        txtname.Text = ""
+        txtDesc.Text = ""
+    End Sub
+
+    Private Sub dgvtask_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvtask.CellClick
+        If dgvtask.Rows.Count = 0 Then Exit Sub
+
+        txtname.Text = dgvtask.CurrentRow.Cells(1).Value.ToString
+        txtDesc.Text = dgvtask.CurrentRow.Cells(2).Value.ToString
+        txtname.Enabled = True
+        txtDesc.Enabled = True
+
+        btncreate.Text = "Create"
+
+    End Sub
+#End Region
+
+End Class
